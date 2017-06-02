041a172153982a5269ef1053f9b9e7d0dca80318
Rob Walsh <brombomb@gmail.com>
2017-06-02 13:23:58 -0600

    Fact of the Day

---
+++ index.js
@@ -0,0 +1,88 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
+'use strict';
+
+const Alexa = require('alexa-sdk');
+var http = require('http');
+
+const APP_ID = ''; // Optional -- let's let this be open for any skill
+var SKILL_NAME = "Today's Fact";
+
+function api(cb) {
+
+	var today = new Date();
+	var factDay = (today.getMonth() + 1) + '/' + today.getDate();
+    
+    return http.get({
+        host: 'numbersapi.com',
+        path:  '/' + factDay + '/date'
+    }, function(res) {
+        res.setEncoding('utf8');
+        // Continuously update stream with data
+        var body = '';
+        res.on('data', function(d) {
+            body += d;
+        });
+        res.on('end', function() {
+
+            try {
+                console.log(body);
+                cb(body);
+            } catch (err) {
+                console.error('Unable to parse response as JSON', err);
+                throw(err);
+            }
+        });
+    }).on('error', function(err) {
+        // handle errors with the request itself
+        console.error('Error with the request:', err.message);
+        throw(err);
+    });
+
+}
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+
+        api(function(fact) {
+            var cardTitle = "Today's Fact";
+            var shouldEndSession = true;
+            var speechOutput = fact;
+            
+            this.emit(':tellWithCard', speechOutput, "Today's Fact", fact);
+                
+        }.bind(this));
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = "You can say tell me a fact, or, you can say exit... What can I help you with?";
+        var reprompt = "What can I help you with?";
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    }
+};
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
